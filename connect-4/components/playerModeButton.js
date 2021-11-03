de4ec4efd483682be615b8bef37657aa8a823fe6
Taesok Kwon <kwontaesok1997@gmail.com>
2021-11-03 00:12:29 -0700

    change fontawesome to react-icons
    アイコンが出たり出なかったりするバグがあったため.

--- connect-4/components/playerModeButton.js
+++ connect-4/components/playerModeButton.js
@@ -2,8 +2,7 @@ import React from "react";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import { Button } from "@material-ui/core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { GiTabletopPlayers } from "react-icons/gi";
 
 export default function AiModeButton() {
   return (
@@ -14,11 +13,9 @@ export default function AiModeButton() {
           color="primary"
           size="large"
           className={styles.btn}
-          style={{ fontSize: "30px" }}
+          style={{ fontSize: "40px" }}
         >
-          <FontAwesomeIcon icon={faUser} className={styles.btn_icon_user} />
-          vs
-          <FontAwesomeIcon icon={faUser} className={styles.btn_icon_user} />
+          <GiTabletopPlayers/>
         </Button>
       </Link>
     </>

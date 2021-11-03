c4adbcb4a43187d7f644d596dd2ea84acd4139ce
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2021-11-04 08:23:52 +0900

    colors.jsを作成。

---
+++ connect-4/pages/colors.js
@@ -0,0 +1,158 @@
+export default {
+  blue: {
+    circle1: {
+      color: "#000092",
+      colorString: "blue",
+    },
+    circle2: {
+      id: "gradient13",
+      startColor: "#0000b1",
+      endColor: "#7e7efa",
+    },
+    circle3: {
+      color: "#202079",
+    },
+    circle4: {
+      id: "gradient14",
+      startColor: "#d3d3fe",
+      middleColor: "#0000cc",
+      endColor: "#0000b4",
+    },
+    circle5: {
+      id: "gradient15",
+      startColor: "#8c8cfa",
+      middleColor: "#0000ab",
+      endColor: "#0000db",
+    },
+  },
+  red: {
+    circle1: {
+      color: "#920000",
+      colorString: "red",
+    },
+    circle2: {
+      id: "gradient04",
+      startColor: "#b10000",
+      endColor: "#fa7e7e",
+    },
+    circle3: {
+      color: "#792020",
+    },
+    circle4: {
+      id: "gradient05",
+      startColor: "#fed3d3",
+      middleColor: "#cc0000",
+      endColor: "#b40000",
+    },
+    circle5: {
+      id: "gradient06",
+      startColor: "#fa8c8c",
+      middleColor: "#ab0000",
+      endColor: "#db0000",
+    },
+  },
+  yellow: {
+    circle1: {
+      color: "#d8a200",
+      colorString: "yellow",
+    },
+    circle2: {
+      id: "gradient07",
+      startColor: "#ffb904",
+      endColor: "#ffe66d",
+    },
+    circle3: {
+      color: "#e2ac00",
+    },
+    circle4: {
+      id: "gradient08",
+      startColor: "#fff9cf",
+      middleColor: "#fab408",
+      endColor: "#e5af00",
+    },
+    circle5: {
+      id: "gradient09",
+      startColor: "#fee885",
+      middleColor: "#e9b318",
+      endColor: "#e7a100",
+    },
+  },
+  green: {
+    circle1: {
+      color: "#009200",
+      colorString: "green",
+    },
+    circle2: {
+      id: "gradient10",
+      startColor: "#00a000",
+      endColor: "#7efa7e",
+    },
+    circle3: {
+      color: "#207920",
+    },
+    circle4: {
+      id: "gradient11",
+      startColor: "#d3fed3",
+      middleColor: "#00bb00",
+      endColor: "#00a300",
+    },
+    circle5: {
+      id: "gradient12",
+      startColor: "#8cfa8c",
+      middleColor: "#009a00",
+      endColor: "#00ca00",
+    },
+  },
+  pink: {
+    circle1: {
+      color: "#a00097",
+      colorString: "pink",
+    },
+    circle2: {
+      id: "gradient10",
+      startColor: "#c000ad",
+      endColor: "#d617ca",
+    },
+    circle3: {
+      color: "#c215a9",
+    },
+    circle4: {
+      id: "gradient11",
+      startColor: "#ffd7ff",
+      middleColor: "#df00cc",
+      endColor: "#c500aa",
+    },
+    circle5: {
+      id: "gradient12",
+      startColor: "#ff8eff",
+      middleColor: "#ba00a6",
+      endColor: "#f200d9",
+    },
+  },
+  cyan: {
+    circle1: {
+      color: "#009593",
+      colorString: "cyan",
+    },
+    circle2: {
+      id: "gradient13",
+      startColor: "#00a9b2",
+      endColor: "#00c8c6",
+    },
+    circle3: {
+      color: "#00a9b4",
+    },
+    circle4: {
+      id: "gradient14",
+      startColor: "#cfffff",
+      middleColor: "#00c7ce",
+      endColor: "#00a8b7",
+    },
+    circle5: {
+      id: "gradient15",
+      startColor: "#6dfdff",
+      middleColor: "#00a3ac",
+      endColor: "#00d5e0",
+    },
+  },
+};

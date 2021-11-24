2b828147112e6efce8a2a870be1a069642d19e32
Taesok Kwon <kwontaesok1997@gmail.com>
2021-11-24 12:20:23 -0800

    create gamePage entryPoint file

--- connect-4/components/gamePageComp/gamePage.js
+++ connect-4/components/gamePageComp/gamePage.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import { Grid } from '@material-ui/core'
+import { Grid, Button } from '@material-ui/core'
 
 import PlayersTurn from './playersTurn'
-import Board from './board'
+import { GrPowerReset } from 'react-icons/gr'
+import { RiTimerLine } from 'react-icons/ri'
+import styles from '../../styles/Home.module.css'
+import CreateBoard from './createBoard'
 
 const GamePage = () => {
+  const resetBoard = () => {
+    console.log("reset!");
+  }
+
   return (
     <div>
       <Grid container direction="row" justifyContent="space-around" alignContent="center">
@@ -12,10 +19,20 @@ const GamePage = () => {
           <PlayersTurn />
         </Grid>
         <Grid>
-          <Board />
+          <CreateBoard />
         </Grid>
         <Grid>
-          Timer and Reset Button
+          <RiTimerLine />
+          <p>00:00</p>
+          <Button 
+            variant="contained"
+            color="primary"
+            size="large"
+            className={styles.btn}
+            style={{ fontSize: "20px", marginTop: "4rem" }}
+            onClick={resetBoard}>
+            <GrPowerReset />
+          </Button>
         </Grid>
       </Grid>
     </div>

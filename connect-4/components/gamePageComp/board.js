db8eb3f9f4253da8a8e181bdc52b2a0a2beb085e
Taesok Kwon <kwontaesok1997@gmail.com>
2021-11-28 01:52:37 -0800

    create ballSetters(button) and board flame

---
+++ connect-4/components/gamePageComp/board.js
@@ -0,0 +1,31 @@
+import React, {useContext} from 'react'
+import AppContext from '../../contexts/AppContext';
+import styles from '../../styles/Home.module.css';
+import BallRedSVG from '../svgFiles/ballRedSVG';
+import TemplateBallSVG from '../svgFiles/templateBallSVG';
+import BallRedMinSVG from '../../public/images/ball-red.min.svg';
+
+// ball-red.min.svg
+const Board = () => {
+  const { state } = useContext(AppContext);
+
+  return (
+    <div className={styles.bgColor}>
+      {state.board.map((row, rowIndex) => {
+        return(
+          <div style={{display: "flex"}}>
+            {row.map((col, colIndex) => {
+              return(
+                <div className={styles.ball}>
+                  {/* <BallRedMinSVG /> */}
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Board;

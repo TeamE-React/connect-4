a9f28fdeccea07ea155e6dddaeb7c1142b4d40e0
Taesok Kwon <kwontaesok1997@gmail.com>
2021-11-28 01:55:56 -0800

    create board flame with 2d array

--- connect-4/components/gamePageComp/createBoard.js
+++ connect-4/components/gamePageComp/createBoard.js
@@ -1,22 +1,29 @@
-import React, {useContext} from 'react'
+import React, { useContext } from "react";
 
-import AppContext from '../../contexts/AppContext';
-import { Button } from "@material-ui/core";
+import AppContext from "../../contexts/AppContext";
+import { Button, Box } from "@material-ui/core";
+import BallSetters from "../ballSetters";
+import Board from '../gamePageComp/board';
 
 const CreateBoard = () => {
-
   const getBoard = () => {
     console.log(state.board);
     console.log(state.currentPlayer);
-  }
+  };
 
   const { state } = useContext(AppContext);
 
   return (
-    <Button onClick={getBoard}>
-      Click
-    </Button>
-  )
-}
+    <div>
+      {/* <Button onClick={getBoard}>Click</Button> */}
+      {state.board.map((col, colIndex) => {
+        return(
+          <BallSetters key={colIndex} colIndex={colIndex}/>
+        )
+        })}
+      <Board />
+    </div>
+  );
+};
 
 export default CreateBoard;

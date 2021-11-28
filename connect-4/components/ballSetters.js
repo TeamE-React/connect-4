8d1ab4999e1897d7f431d3581131d0ff0ace9f25
Taesok Kwon <kwontaesok1997@gmail.com>
2021-11-28 01:55:10 -0800

    create ballSetters(button) and setBall function

--- connect-4/components/ballSetters.js
+++ connect-4/components/ballSetters.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useContext} from "react";
 
-const BallSetters = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+import { Button, Box } from "@material-ui/core";
+import { GoTriangleDown } from "react-icons/go";
+import styles from "../styles/Home.module.css";
+import AppContext from "../contexts/AppContext";
+
+const BallSetters = ({ colIndex }) => {  
+  const { state, dispatch } = useContext(AppContext);
+
+  const setBall = () => {
+    console.log(state.currentPlayer.color);
+    let color = state.currentPlayer.color;
+    console.log(colIndex);
+    dispatch({type: 'SET_BALL', color, colIndex, rowIndex: 0});
+  }
+
+  return (
+    <Button
+        variant="contained"
+        color="secondary"
+        size="large"
+        className={styles.btn}
+        style={{ fontSize: "20px", margin: "16px" }}
+        onClick={setBall}
+        >
+        < GoTriangleDown />
+    </Button>
+  );
+};
 
 export default BallSetters;

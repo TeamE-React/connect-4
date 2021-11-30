4d25a57e882c129e3ba3d05f99bb5eac302067c9
Taesok Kwon <kwontaesok1997@gmail.com>
2021-11-30 03:11:09 -0800

    add class judge to evaluate winner

---
+++ connect-4/model/judge.js
@@ -0,0 +1,155 @@
+import { Config } from "../config";
+import { lastBallPosition } from "./index";
+
+export class Judge{
+  constructor(board, rowIndex, colIndex){
+    this.board = board;
+    this.currentBall = board[rowIndex][colIndex];
+    this.lastBallPosition = new lastBallPosition(rowIndex, colIndex, this.currentBall.color);
+    this.stack = 1;
+  }
+
+  checkWinner(){
+    const rowIndex = this.lastBallPosition.rowIndex;
+    const colIndex = this.lastBallPosition.colIndex;
+    return(
+      this.verticalCheck(rowIndex, colIndex) || this.horizontalCheck(rowIndex, colIndex) || this.rightDiagonalCheck(rowIndex, colIndex) || this.leftDiagonalCheck(rowIndex, colIndex)
+    );
+  }
+
+  check(n){
+    if(n >= Config.board.size.min){ // 4
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  isBothSameColor(ball1, ball2){
+    if(ball1.color === null || ball2.color === null || ball1.color != ball2.color) return false;
+    else return true;
+  }
+
+  verticalCheck(rowIndex, colIndex){
+    this.stack = 1;
+
+    // when the ball is dropped, there should be nothing on the top of the ball, so we only need lower check
+    const lowerCheck = () => {
+      // lower limit
+      const limit = this.board.length; // rowIndex + 3
+      for(let row = rowIndex+1; row < limit; row++){
+        const ball = this.board[row][colIndex];
+        if(this.isBothSameColor(ball, this.currentBall)) this.stack++;
+      }
+    }
+    lowerCheck();
+
+    return this.checkWinner(this.stack);
+  }
+
+  horizontalCheck(rowIndex, colIndex){
+    this.stack = 1;
+
+    const leftCheck = () => {
+      // left limit
+      const limit = 0; // rowIndex - 3
+      for(let col = colIndex-1; col >= limit; col--){
+        const ball = this.board[rowIndex][col];
+        if(this.isBothSameColor(ball, this.currentBall)) this.stack++;
+      }
+    }
+
+    const rightCheck = () => {
+      // right limit
+      const limit = this.board.length; // rowIndex + 3
+      for(let col = colIndex+1; col < limit; col++){
+        const ball = this.board[rowIndex][col];
+        if(this.isBothSameColor(ball, this.currentBall)) this.stack++;
+      }
+    }
+
+    leftCheck();
+    rightCheck();
+
+    return this.check(this.stack);
+  }
+
+  rightDiagonalCheck(rowIndex, colIndex){
+    this.stack = 1;
+
+    const upperCheck = () => {
+      const verticalLimit = 0; // rowIndex - 3
+      const horizontalLimit = this.board.length; // colIndex + 3
+
+      let row = rowIndex-1;
+      let col = colIndex+1;
+
+      while(col < horizontalLimit && row >= verticalLimit){
+        const ball = this.board[row][col];
+        if(this.isBothSameColor(ball, this.currentBall)) this.stack++;
+        row--;
+        col++;
+      }
+    }
+
+    const lowerCheck = () => {
+      const verticalLimit = this.board.length; // rowIndex + 3
+      const horizontalLimit = 0; // colIndex - 3
+
+      let row = rowIndex+1;
+      let col = colIndex-1;
+
+      while(col >= horizontalLimit && row < verticalLimit){
+        const ball = this.board[row][col];
+        if(this.isBothSameColor(ball, this.currentBall)) this.stack++;
+        row++;
+        col--;
+      }
+    }
+
+    upperCheck();
+    lowerCheck();
+
+    return this.check(this.stack);
+  }
+
+  leftDiagonalCheck(rowIndex, colIndex){
+    this.stack = 1;
+
+    const upperCheck = () => {
+      const verticalLimit = 0; // rowIndex - 3
+      const horizontalLimit = 0; // colIndex - 3
+
+      let row = rowIndex-1;
+      let col = colIndex-1;
+
+      while(col >= horizontalLimit && row >= verticalLimit){
+        const ball = this.board[row][col];
+        if(this.isBothSameColor(ball, this.currentBall)) this.stack++;
+        row--;
+        col--;
+      }
+    }
+
+    const lowerCheck = () => {
+      const verticalLimit = this.board.length; // rowIndex + 3
+      const horizontalLimit = this.board.length; // colIndex + 3
+
+      let row = rowIndex+1;
+      let col = colIndex+1;
+
+      while(col < horizontalLimit && row < verticalLimit){
+        const ball = this.board[row][col];
+        if(this.isBothSameColor(ball, this.currentBall)) this.stack++;
+        row++;
+        col++;
+      }
+    }
+
+    upperCheck();
+    lowerCheck();
+
+    return this.check(this.stack);
+  }
+}
